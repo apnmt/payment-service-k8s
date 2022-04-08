1a1dceebe12e12a1a98fa660b67d09375e35b55d
tobi5775 <tobias.wittmann96@web.de>
2022-04-08 14:32:20 +0200

    log sending kafka event

--- src/main/java/de/apnmt/payment/kafka/OrganizationActivationEventSender.java
+++ src/main/java/de/apnmt/payment/kafka/OrganizationActivationEventSender.java
@@ -3,11 +3,15 @@ package de.apnmt.payment.kafka;
 import de.apnmt.common.event.ApnmtEvent;
 import de.apnmt.common.event.value.OrganizationActivationEventDTO;
 import de.apnmt.common.sender.ApnmtEventSender;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class OrganizationActivationEventSender implements ApnmtEventSender<OrganizationActivationEventDTO> {
+
+    private static final Logger log = LoggerFactory.getLogger(OrganizationActivationEventSender.class);
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
@@ -17,6 +21,7 @@ public class OrganizationActivationEventSender implements ApnmtEventSender<Organ
 
     @Override
     public void send(String topic, ApnmtEvent<OrganizationActivationEventDTO> event) {
+        log.info("Send event {} to topic {}", event, topic);
         this.kafkaTemplate.send(topic, event);
     }
 
